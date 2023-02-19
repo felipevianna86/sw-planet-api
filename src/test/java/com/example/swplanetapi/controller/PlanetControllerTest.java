db74c0558ff438a06544c677f48268a987a23eba
felipevianna86 <felipe.vianna86@gmail.com>
2023-02-19 20:05:51 -0300

    Teste de integração: Teste de sucesso PlanetaController

---
+++ src/test/java/com/example/swplanetapi/controller/PlanetControllerTest.java
@@ -0,0 +1,39 @@
+package com.example.swplanetapi.controller;
+
+import com.example.swplanetapi.service.PlanetService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static com.example.swplanetapi.common.PlanetConstants.PLANET;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(PlanetController.class)
+public class PlanetControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private PlanetService planetService;
+    @Test
+    public void create() throws Exception {
+
+        Mockito.when(planetService.create(PLANET)).thenReturn(PLANET);
+
+        mockMvc.perform(post("/planet").content(objectMapper.writeValueAsString(PLANET))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value(PLANET));
+    }
+}
